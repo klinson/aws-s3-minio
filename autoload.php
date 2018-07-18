023fb437f6a01232a817b031cb05fa7070ce8d87
klinson <klinson@163.com>
2018-07-18 11:07:29 +0800

    commit autoload

---
+++ autoload.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: klinson <klinson@163.com>
+ * Date: 18-7-18
+ * Time: 上午10:05
+ */
+
+require __DIR__.'/src/Autoloader.php';
+
+Minio\Autoloader::register();
